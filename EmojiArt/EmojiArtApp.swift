82f17602afc77a1c61cbe1ddd233c97509cdb42c
Gabriel Miranda <silva.gabrielmiranda@gmail.com>
2022-04-10 13:06:42 -0300

    Initial Commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Gabriel Miranda on 10/04/22.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
